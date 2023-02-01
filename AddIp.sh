2266e71420775567aa068cf97e9a4df4d740d3b4
racym <rassim@example.com>
2023-02-01 15:04:07 +0100

    Second Commit

---
+++ AddIp.sh
@@ -0,0 +1,15 @@
+echo "OnLine IP adresses: " > online_ip.txt
+echo "OffLine IP adresses: " > offline_ip.txt
+
+addrip=10.93.161.45
+base=${addrip:0:10}
+
+# Loop through the IP range and run ping to scan for open ports
+for i in {0..255..1}
+do
+	if ping -W 1 -c 1 $base$i
+	then echo $base$i >> online_ip.txt
+	else echo $base$i >> offline_ip.txt
+	fi
+done
+
